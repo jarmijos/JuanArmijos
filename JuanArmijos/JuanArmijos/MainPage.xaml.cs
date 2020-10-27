d493024fc0efecad0f406e9aedc41dfd37e092b0
SISTEMAS <SISTEMAS@DESKTOP-97DSV6N>
2020-10-26 22:09:54 -0500

    Agregar archivos de proyecto.

---
+++ JuanArmijos/JuanArmijos/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JuanArmijos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUser.Text.Equals("estudiante2020") && txtPass.Text.Equals("uisrael2020"))
+                {
+                    string user = txtUser.Text;
+                    await Navigation.PushModalAsync(new Registrar(user));
+                }
+                else
+                {
+                    DisplayAlert("Error", "Usuario o contraseña incorrectos", "Reintentar");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Error", ex.ToString(), "Cerrar");
+            }
+
+        }
+    }
+}
---
+++ JuanArmijos/JuanArmijos/Registrar.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JuanArmijos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registrar : ContentPage
+    {
+        public Registrar(string user)
+        {
+            InitializeComponent(); 
+            lblUser.Text = "Bienvenido "+user;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            double valorInicial = double.Parse(txtMonto.Text); 
+            double final = (valorInicial / 3) * 1.05;
+            lblCuota.Text = final.ToString();
+            
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            
+            if (lblCuota.Text.Equals(""))
+            {
+                DisplayAlert("Error", "Por favor primero calcule el monto inical a pagar", "Reintentar");
+            }
+            else
+            {
+                string nombre = txtNombre.Text;
+                string user = lblUser.Text;
+                double valor = double.Parse(lblCuota.Text);
+                double total = valor * 3;
+                await Navigation.PushModalAsync(new Resumen(user,total,nombre));
+            }
+            
+        }
+    }
+}
